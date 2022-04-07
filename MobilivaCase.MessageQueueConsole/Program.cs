59cd2a634e446a0f0688dfcefce1c4a18f0e56be
mozer02 <m_ozer_02@yahoo.com>
2022-04-07 07:44:30 +0300

    Adding the Message Quene Console Project

---
+++ MobilivaCase.MessageQueueConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobilivaCase.MessageQueueConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
